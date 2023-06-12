70d025f0df7ef69b1fb2ff382641053dea55300a
Sarah Risley <sarah.risley@hotmail.com>
2023-06-12 16:41:25 +0100

    moved phase 2 exercises and challenges into phase 2 folder

--- lib/count_words.py
+++
@@ -1,6 +0,0 @@
-def count_words(str):
-    result = str.split(' ')
-    if len(str) < 1:
-        return 0
-    else:
-        return len(result)
--- tests/test_count_words.py
+++
@@ -1,9 +0,0 @@
-from lib.count_words import *
-
-def test_count_four_words():
-    result = count_words("This is four words")
-    assert result == 4
-
-def test_count_empty_string():
-    result = count_words("")
-    assert result == 0
